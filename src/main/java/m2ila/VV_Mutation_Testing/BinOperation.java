e2a9d5c64b97912321740a7237b53322c4086dad
masoumiaa <aminesoumiaa@gmail.com>
2017-11-07 14:49:34 +0100

    Add binary operations classes

---
+++ src/main/java/m2ila/VV_Mutation_Testing/BinOperation.java
@@ -0,0 +1,11 @@
+package m2ila.VV_Mutation_Testing;
+
+public interface BinOperation {
+
+	void setA(double a);
+	void setB(double b);
+	double getA();
+	double getB();
+	double operation();
+	void print();
+}
