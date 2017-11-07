183a6b07e1fbf7087ce9321643c7e1348a8b4b33
masoumiaa <aminesoumiaa@gmail.com>
2017-11-07 15:30:47 +0100

    update binops unit tests

--- src/test/java/m2ila/VV_Mutation_Testing/BinOperationTest.java
+++ src/test/java/m2ila/VV_Mutation_Testing/BinOperationTest.java
@@ -9,25 +9,25 @@ public class BinOperationTest {
 
 	@Test
 	public void AdditionTest1(){
-		Addition add = new Addition(1, 1);
-		Assert.assertEquals(add.operation(),2);
+		Addition add = new Addition(1.0, 1.0);
+		Assert.assertEquals(add.operation(),2.0);
 	}
 	
 	@Test
 	public void SubstractionTest1(){
-		Substraction sub = new Substraction(4, 2);
-		Assert.assertEquals(sub.operation(),2);
+		Substraction sub = new Substraction(4.0, 2.0);
+		Assert.assertEquals(sub.operation(),2.0);
 	}
 	
 	@Test
 	public void MultiplicationTest1(){
-		Multiplication mult = new Multiplication(2, 2);
-		Assert.assertEquals(mult.operation(),4);
+		Multiplication mult = new Multiplication(2.0, 2.0);
+		Assert.assertEquals(mult.operation(),4.0);
 	}
 	
 	@Test
 	public void DivisionTest1(){
-		Division div = new Division(4, 2);
-		Assert.assertEquals(div.operation(),2);
+		Division div = new Division(4.0, 2.0);
+		Assert.assertEquals(div.operation(),2.0);
 	}
 }
