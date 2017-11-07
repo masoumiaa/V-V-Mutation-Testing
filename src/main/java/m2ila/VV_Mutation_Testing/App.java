78393a357a121643ff1171fbdbfb73416aada91c
masoumiaa <aminesoumiaa@gmail.com>
2017-11-07 15:28:59 +0100

    Add main tests for binops

--- src/main/java/m2ila/VV_Mutation_Testing/App.java
+++ src/main/java/m2ila/VV_Mutation_Testing/App.java
@@ -1,5 +1,10 @@
 package m2ila.VV_Mutation_Testing;
 
+import m2ila.VV_Mutation_Testing.BinOp.Addition;
+import m2ila.VV_Mutation_Testing.BinOp.Division;
+import m2ila.VV_Mutation_Testing.BinOp.Multiplication;
+import m2ila.VV_Mutation_Testing.BinOp.Substraction;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Addition add = new Addition(1.0, 1.0);
+        add.operation();
+        add.print();
+        
+        Substraction sub = new Substraction(2.0, 1.0);
+        sub.operation();
+        sub.print();
+        
+        Multiplication mult = new Multiplication(2.0, 2.0);
+        mult.operation();
+        mult.print();
+        
+        Division div = new Division(4.0, 2.0);
+        div.operation();
+        div.print();
     }
 }
