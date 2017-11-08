c56e1c9cbba0fd925ca7e7ff408d0e11a72a47bb
masoumiaa <aminesoumiaa@gmail.com>
2017-11-08 22:33:03 +0100

    Loading the class + Reading methods names

--- Mutation-Testing/src/main/java/m2ila/vv/Mutation_Testing/App.java
+++ Mutation-Testing/src/main/java/m2ila/vv/Mutation_Testing/App.java
@@ -2,6 +2,8 @@ package m2ila.vv.Mutation_Testing;
 
 import javassist.CannotCompileException;
 import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
 import javassist.Loader;
 import javassist.NotFoundException;
 import javassist.Translator;
@@ -14,29 +16,20 @@ public class App
         try {
         	// WIP 
         	// Transformation
+        	// Creating container
         	ClassPool pool = ClassPool.getDefault();
-        	Loader loader = new Loader(pool);
-        	
-        	Translator logger = new Translator() {
-
-				public void onLoad(ClassPool pool, String classname) throws NotFoundException, CannotCompileException {
-					System.out.println(classname);
-					// Transformation :modifier la classe 
-				}
-
-				public void start(ClassPool arg0) throws NotFoundException, CannotCompileException {
-					System.out.println("Starting...");
-					
-				}
-        	};
-        	
-        	loader.addTranslator(pool, logger);
         	pool.appendClassPath("/home/aminesoumiaa/workspace/VV-Mutation-Testing/inputs/target/classes");
-        	// TODO delete this line 
-        	loader.run("m2ila.vv.inputs.App", args);
         	
-			
-           // throw new Throwable();
+        	//creating compile-time class
+        	CtClass ctClass = pool.get("m2ila.vv.inputs.BinOperations");
+        	//Getting all methodes 
+        	final CtMethod[] existingMethods = ctClass.getDeclaredMethods();
+        	for (CtMethod ctMethod : existingMethods) {
+        		//printing methodes names 
+        		// TODO replace prints with reporting
+				System.out.println(ctMethod.getName());
+			}
+        	
         }
 
         catch(Throwable exc) {
