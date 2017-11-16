571567a3f1c30707ea9296f350046374b637e05c
masoumiaa <aminesoumiaa@gmail.com>
2017-11-16 09:50:05 +0100

    upgrade juint version + change junit tests

--- inputs/src/test/java/m2ila/vv/inputs/CompOperationTest.java
+++ inputs/src/test/java/m2ila/vv/inputs/CompOperationTest.java
@@ -1,8 +1,7 @@
 package m2ila.vv.inputs;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
-
-import junit.framework.Assert;
 import m2ila.vv.inputs.ComparisonOperations;
 
 public class CompOperationTest {
@@ -10,41 +9,41 @@ public class CompOperationTest {
 	ComparisonOperations compOp = new ComparisonOperations();
 	@Test
 	public void LowerTest1(){
-		Assert.assertTrue(compOp.Lower(1.0, 3.0));
+		assertTrue(compOp.Lower(1.0, 3.0));
 	}
 	
 	@Test
 	public void LowerTest2(){
-		Assert.assertFalse(compOp.Lower(1.0, 1.0));
+		assertFalse(compOp.Lower(1.0, 1.0));
 	}
 	
 	@Test
 	public void LowerOrEqualTest1(){
-		Assert.assertTrue(compOp.LowerOrEqual(1.0, 1.0));
+		assertTrue(compOp.LowerOrEqual(1.0, 1.0));
 	}
 	
 	@Test
 	public void LowerOrEqualTest2(){
-		Assert.assertFalse(compOp.LowerOrEqual(1.0, 0.0));
+		assertFalse(compOp.LowerOrEqual(1.0, 0.0));
 	}
 	
 	@Test
 	public void HigherTest1(){
-		Assert.assertTrue(compOp.Higher(5.0, 3.0));
+		assertTrue(compOp.Higher(5.0, 3.0));
 	}
 	
 	@Test
 	public void HigherTest2(){
-		Assert.assertFalse(compOp.Higher(3.0, 3.0));
+		assertFalse(compOp.Higher(3.0, 3.0));
 	}
 	
 	@Test
 	public void HigherOrEqualTest1(){
-		Assert.assertTrue(compOp.HigherOrEqual(5.0, 3.0));
+		assertTrue(compOp.HigherOrEqual(5.0, 3.0));
 	}
 	
 	@Test
 	public void HigherOrEqualTest2(){
-		Assert.assertFalse(compOp.HigherOrEqual(3.0, 5.0));
+		assertFalse(compOp.HigherOrEqual(3.0, 5.0));
 	}
 }
