4ea06015207844ebbea951d394fa84dd91e52958
Mhamed-Amine Soumiaa <mhamed-amine.soumiaa@sogeti.com>
2017-11-20 16:50:09 +0100

    Add Report Generator

---
+++ Mutation-Testing/src/main/java/m2ila/vv/Mutation_Testing/Reporter.java
@@ -0,0 +1,35 @@
+package m2ila.vv.Mutation_Testing;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Reporter {
+	private PrintWriter pw;
+	private StringBuilder sb;
+	
+	public void init() throws FileNotFoundException{
+		String fileName = generateFileName();
+        pw = new PrintWriter(new File(fileName));
+        sb = new StringBuilder();
+	}
+	
+	public void insert(String str){
+		this.sb.append(str);
+		this.sb.append('\n');
+	}
+	
+	public void save(){
+        pw.write(sb.toString());
+        pw.close();
+        System.out.println("Report done!");
+	}
+	
+	public String generateFileName(){
+		String time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+		String fileName = "Report"+time+".html";
+		return fileName;
+	}
+}
